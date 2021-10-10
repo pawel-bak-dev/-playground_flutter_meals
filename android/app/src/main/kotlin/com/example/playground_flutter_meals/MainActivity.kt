3e9f80188818d0a1d5beb5dedcf5f72a31fdc912
pawel-bak-dev <pawel.bak.dev@gmail.com>
2021-10-10 22:27:12 +0200

    init project

---
+++ android/app/src/main/kotlin/com/example/playground_flutter_meals/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.playground_flutter_meals
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'PabloMeals',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('PabloMeals'),
+      ),
+      body: Center(
+        child: Text(
+          'Lets start!',
+        ),
+      ),
+    );
+  }
+}
